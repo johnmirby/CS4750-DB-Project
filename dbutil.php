374f1739ffee4c12416886c2829148347878a82a
Rock Beom Kim <purasle@gmail.com>
2016-03-23 13:25:36 -0400

    Basic dbutil stuff

---
+++ dbutil.php
@@ -0,0 +1,21 @@
+<?php
+class DbUtil{
+	public static $loginUser = "cs4750jmi8fs";
+	public static $loginPass = "mtgproject";
+	public static $host = "stardock.cs.virginia.edu"; // DB Host
+	public static $schema = "cs4750jmi8fs"; // DB Schema
+
+	public static function loginConnection(){
+		$db = new mysqli(DbUtil::$host, DbUtil::$loginUser, DbUtil::$loginPass, DbUtil::$schema);
+
+		if($db->connect_errno){
+			echo("Could not connect to db");
+			$db->close();
+			exit();
+		}
+
+		return $db;
+	}
+
+}
+?>
