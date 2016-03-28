3ccc24c94146ebe845d02296f20af6247671ffbd
johnmirby <jmi8fs@virginia.edu>
2016-03-27 21:43:40 -0400

    Added search script

---
+++ searchCardName.php
@@ -0,0 +1,24 @@
+<?php
+        require "dbutil.php";
+        $db = DbUtil::loginConnection();
+
+        $stmt = $db->stmt_init();
+
+        if($stmt->prepare("select Card_Name from Card where Card_Name like ?") or die(mysqli_error($db))) {
+                $searchString = '%' . $_GET['searchCardName'] . '%';
+                $stmt->bind_param(s, $searchString);
+                $stmt->execute();
+                $stmt->bind_result($Card_Name);
+                echo "<table border=1><th>Card Name</th>\n";
+                while($stmt->fetch()) {
+                        echo "<tr><td>$Card_Name</td></tr>";
+                }
+                echo "</table>";
+
+                $stmt->close();
+        }
+
+        $db->close();
+
+
+?>
