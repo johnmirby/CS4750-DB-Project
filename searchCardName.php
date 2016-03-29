2088d36dde3a2ce18ff5ead1484e9c17cf6bd33e
johnmirby <jmi8fs@virginia.edu>
2016-03-29 18:10:25 -0400

    Added filters for rules text and creature dropdown currently

--- searchCardName.php
+++ searchCardName.php
@@ -4,18 +4,39 @@
 
         $stmt = $db->stmt_init();
 
-        if($stmt->prepare("select Card_Name from Card where Card_Name like ?") or die(mysqli_error($db))) {
-                $searchString = '%' . $_GET['searchCardName'] . '%';
-                $stmt->bind_param(s, $searchString);
-                $stmt->execute();
-                $stmt->bind_result($Card_Name);
-                echo "<table border=1><th>Card Name</th>\n";
-                while($stmt->fetch()) {
-                        echo "<tr><td>$Card_Name</td></tr>";
-                }
-                echo "</table>";
+        $typeIndex = $_GET['typeIndex'];
 
-                $stmt->close();
+        if ($typeIndex == 0) {
+                if($stmt->prepare("select * from Card where Card_Name like ? and Rules_Text like ?") or die(mysqli_error($db))) {
+                        $searchCardNameString = '%' . $_GET['searchCardName'] . '%';
+                        $searchCardRulesTextString = '%' . $_GET['searchCardRulesText'] . '%';
+                        $stmt->bind_param('ss', $searchCardNameString, $searchCardRulesTextString);
+                        $stmt->execute();
+                        $stmt->bind_result($Card_Name, $Rules_Text);
+                        echo "<table border=1><th>Card Name</th><th>Rules Text</th>\n";
+                        while($stmt->fetch()) {
+                                echo "<tr><td>$Card_Name</td><td>$Rules_Text</td></tr>";
+                        }
+                        echo "</table>";
+
+                        $stmt->close();
+                }
+        }
+        elseif ($typeIndex == 1) {
+                if($stmt->prepare("select * from Card natural join Creature where Card_Name like ? and Rules_Text like ?") or die(mysqli_error($db))) {
+                        $searchCardNameString = '%' . $_GET['searchCardName'] . '%';
+                        $searchCardRulesTextString = '%' . $_GET['searchCardRulesText'] . '%';
+                        $stmt->bind_param('ss', $searchCardNameString, $searchCardRulesTextString);
+                        $stmt->execute();
+                        $stmt->bind_result($Card_Name, $Rules_Text, $Mana_Cost, $Power, $Subtype, $Toughness, $Legendary);
+                        echo "<table border=1><th>Card Name</th><th>Rules Text</th><th>Mana Cost</th><th>Power</th><th>Toughness</th><th>Subtype</th><th>Legendary</th>\n";
+                        while($stmt->fetch()) {
+                                echo "<tr><td>$Card_Name</td><td>$Rules_Text</td><td>$Mana_Cost</td><td>$Power</td><td>$Toughness</td><td>$Subtype</td><td>$Legendary</td></tr>";
+                        }
+                        echo "</table>";
+
+                        $stmt->close();
+                }
         }
 
         $db->close();
